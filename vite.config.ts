24fae5d88ff091e03d6fb943ba4226bae2074057
setsuna-0201 <setsuna.kunugiza@gmail.com>
2026-04-13 18:31:53 -0700

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/ver2-/',
+  base:'/OsakaMedicalSchool-NurseDay-AED/',
 })
